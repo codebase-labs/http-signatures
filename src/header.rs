376ca7b80667896875124525a369c4fae4892931
dskyberg <davidskyberg@gmail.com>
2021-12-12 11:36:39 -0500

    Added Specialty Components

--- src/header.rs
+++ src/header.rs
@@ -3,6 +3,7 @@ use std::str::FromStr;
 
 use http::header::{HeaderName, InvalidHeaderName};
 
+///
 /// Pseudo-headers are used to incorporate additional information into a HTTP
 /// signature for which there is no corresponding HTTP header.
 ///
@@ -11,6 +12,16 @@ use http::header::{HeaderName, InvalidHeaderName};
 #[derive(Debug, Copy, Clone, PartialOrd, Ord, PartialEq, Eq)]
 #[non_exhaustive]
 pub enum PseudoHeader {
+    /// The signature metadata parameters for this signature. (Section 2.3.1)
+    SignatureParams,
+    /// The method used for a request. (Section 2.3.2)
+    Method,
+    /// The full target URI for a request. (Section 2.3.3)
+    TargetURI,
+    /// The authority of the target URI for a request. (Section 2.3.4)
+    Authority,
+    /// The scheme of the target URI for a request. (Section 2.3.5)
+    Scheme,
     /// The `(request-target)` pseudo-header is constructed by joining the lower-cased
     /// request method (`get`, `post`, etc.) and the request path (`/some/page?foo=1`)
     /// with a single space character.
@@ -18,6 +29,16 @@ pub enum PseudoHeader {
     /// For example:
     /// `get /index.html`
     RequestTarget,
+    /// The absolute path portion of the target URI for a request. (Section 2.3.7)
+    Path,
+    /// The query portion of the target URI for a request. (Section 2.3.8)
+    Query,
+    /// The parsed query parameters of the target URI for a request. (Section 2.3.9)
+    QueryParams,
+    /// The status code for a response. (Section 2.3.10)
+    Status,
+    /// A signature from a request message that resulted in this response message. (Section 2.3.11)
+    RequestResponse,
     /// Passed as part of the auth header
     Created,
     /// Passed as part of the auth header
@@ -28,9 +49,19 @@ impl PseudoHeader {
     /// Returns the string representation of the pseudo-header.
     pub fn as_str(&self) -> &str {
         match self {
-            PseudoHeader::RequestTarget => "(request-target)",
-            PseudoHeader::Created => "(created)",
-            PseudoHeader::Expires => "(expires)",
+            PseudoHeader::SignatureParams => "@signature-params",
+            PseudoHeader::Method => "@method",
+            PseudoHeader::TargetURI => "@target-uri",
+            PseudoHeader::Authority => "@authority",
+            PseudoHeader::Scheme => "@scheme",
+            PseudoHeader::RequestTarget => "@request-target",
+            PseudoHeader::Path => "@path",
+            PseudoHeader::Query => "@query",
+            PseudoHeader::QueryParams => "@query-params",
+            PseudoHeader::Status => "@status",
+            PseudoHeader::RequestResponse => "@request-response",
+            PseudoHeader::Created => "@created",
+            PseudoHeader::Expires => "@expires",
         }
     }
 }
@@ -39,9 +70,19 @@ impl FromStr for PseudoHeader {
     type Err = ();
     fn from_str(s: &str) -> Result<PseudoHeader, Self::Err> {
         match s {
-            "(request-target)" => Ok(PseudoHeader::RequestTarget),
-            "(created)" => Ok(PseudoHeader::Created),
-            "(expires)" => Ok(PseudoHeader::Expires),
+            "@signature-params" => Ok(PseudoHeader::SignatureParams),
+            "@method" => Ok(PseudoHeader::Method),
+            "@target-uri" => Ok(PseudoHeader::TargetURI),
+            "@authority" => Ok(PseudoHeader::Authority),
+            "@scheme" => Ok(PseudoHeader::Scheme),
+            "@request-target" => Ok(PseudoHeader::RequestTarget),
+            "@path" => Ok(PseudoHeader::Path),
+            "@query" => Ok(PseudoHeader::Query),
+            "@query-params" => Ok(PseudoHeader::QueryParams),
+            "@status" => Ok(PseudoHeader::Status),
+            "@request-response" => Ok(PseudoHeader::RequestResponse),
+            "@created" => Ok(PseudoHeader::Created),
+            "@expires" => Ok(PseudoHeader::Expires),
             _ => Err(()),
         }
     }
